23dcdd4e0c3572972e3e3b431e9a36c628ad9d3d
Abhimanyu Singh <rpnsingh0000@gmail.com>
2025-05-01 09:23:43 +0000

    dataType summary

---
+++ Basics/dataType-summary.js
@@ -0,0 +1,39 @@
+ 
+//  Primitive
+
+//  7 types : String , number, boolean, null , undefined, symbol, BigInt
+
+ const score = 100
+ const scoreValue = 100.3
+
+ const isLoggedIn = false
+ const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id == anotherId);
+
+// const bigNumber = 35467889902n
+
+
+
+
+// Reference (non-primitives) 
+
+// types : Array, Objects, Functions
+
+const heros = ["krish", "jaadu", "shaktiman"];
+let myObj = {
+    name: "Abhimanyu",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+
+}
+console.log(typeof bigNumber);
