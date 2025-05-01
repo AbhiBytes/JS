3aa280bc1156157c37c439d1842716c792469a2d
Abhimanyu Singh <rpnsingh0000@gmail.com>
2025-05-01 08:30:17 +0000

    comparisions

---
+++ Basics/comparisions.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// avoid these types of conversion as it gives you inconsistent results
+
+// ===(strict check)  --> it checks the both values and the dataTypes of values 
+console.log("2" === 2);
+
+
+
+
+
