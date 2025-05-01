a4199507651b2daa98ede3e1ce1b770f67e0fce4
Abhimanyu Singh <rpnsingh0000@gmail.com>
2025-05-01 05:31:50 +0000

    conversion of dataTypes

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33ab"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33ab" => NaN
+//true => 1; false => 0;
+
+let isLoggedIn = "Abhi";
+ 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false;
+//"" => false
+//"Abhi" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
