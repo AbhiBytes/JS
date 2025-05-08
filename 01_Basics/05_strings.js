9708e4b962bbf93d313a4300049bdc3158a5095c
Abhimanyu Singh <rpnsingh0000@gmail.com>
2025-05-08 13:22:22 +0000

    more methods

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -1,7 +1,7 @@
 const name = "Abhimanyu"
 const repoCount = 10
 
-// console.log(name + repoCount + " Value"); // Outdated Syntax (avoid it)
+ console.log(name + repoCount + " Value"); // Outdated Syntax (avoid it)
 
 console.log(`Hello my name is  ${name} and  my repo count is ${repoCount}`);
 
@@ -33,6 +33,43 @@ console.log(url.includes("github"));
 console.log(url.includes("lol"));
 
 console.log(gameName.split("-"));
+console.log(newStringOne.anchor());
+
+console.log(newStringOne.at(6));
+console.log(newStringOne.big());
+
+console.log(newStringOne.blink());
+
+console.log(newStringOne.bold());
+console.log(newStringOne.charCodeAt(3));
+
+console.log(newStringOne.concat("singh"));
+console.log(newStringOne.endsWith());
+console.log(newStringOne.fixed());
+console.log(newStringOne.fontcolor());
+console.log(newStringOne.fontsize());
+console.log(newStringOne.includes("z"));
+console.log(newStringOne.isWellFormed());
+console.log(newStringOne.italics());
+console.log(newStringOne.lastIndexOf());
+console.log(newStringOne.link("https://www.hotstar.com/in/onboarding/profile?ref=%2Fin"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
