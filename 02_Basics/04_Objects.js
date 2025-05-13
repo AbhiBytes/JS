629070aa7389cd1a2bd511662ad2fd802774dd0e
Abhimanyu Singh <rpnsingh0000@gmail.com>
2025-05-13 08:52:01 +0000

    Objects

---
+++ 02_Basics/04_Objects.js
@@ -0,0 +1,70 @@
+// const { useSyncExternalStore } = require("react")
+
+// const tinderUser = new Object() // singleton Object
+const tinderUser = {} // non singleton Object
+
+tinderUser.id = "123as"
+tinderUser.name ="Abhi"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "Abhi123@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Abhimanyu",
+            lastname: "Singh"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+
+// const obj3 = Object.assign( {} ,obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const user = [
+    {
+        id: 1,
+        email: "A@gmail.com"
+    },
+     {
+        id: 2,
+        email: "b@gmail.com"
+    },
+     {
+        id: 3,
+        email: "c@gmail.com"
+    },
+
+]
+
+user[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
